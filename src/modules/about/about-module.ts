451df040130209f80d99d1b16ba70172da28b9f0
noqcks <benji@093b.org>
2024-04-07 02:48:55 +0000

    Upgrade dependencies

--- src/modules/about/about-module.ts
+++ src/modules/about/about-module.ts
@@ -1,11 +1,11 @@
 import { Vue as _Vue } from 'vue/types/vue'
 import { VueModule } from 'vue-modules'
-import VueRouter from 'vue-router'
+import { Router } from 'vue-router'
 import { aboutRoutes } from './router/about-routes'
 
 export class AboutModule implements VueModule {
   readonly name = 'about'
-  constructor(private router: VueRouter) {}
+  constructor(private router: Router) {}
 
   install(Vue: typeof _Vue) {
     this.router.addRoutes(aboutRoutes)
