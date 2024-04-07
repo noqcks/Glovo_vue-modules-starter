3eaed81806a1c306a36eebbe03f817553bafc9fc
noqcks <benji@093b.org>
2024-04-07 02:49:24 +0000

    Upgrade dependencies

--- src/modules/core/core-module.ts
+++ src/modules/core/core-module.ts
@@ -1,12 +1,12 @@
 import { Vue as _Vue } from 'vue/types/vue'
 import { VueModule } from 'vue-modules'
-import VueRouter from 'vue-router'
+import { createRouter, Router } from 'vue-router'
 import { Store } from 'vuex'
 import App from './components/App.vue'
 
 export class CoreModule implements VueModule {
   readonly name = 'core'
-  constructor(private router: VueRouter, private store: Store<any>) {}
+  constructor(private router: Router, private store: Store<any>) {}
   install(Vue: typeof _Vue) {
     new Vue({
       router: this.router,
