6a4c269e8a27e6b9093d366ca49567a9ca7a91c1
noqcks <benji@093b.org>
2024-04-07 02:50:18 +0000

    Upgrade dependencies

--- src/modules/router/router-module.ts
+++ src/modules/router/router-module.ts
@@ -1,17 +1,16 @@
 import { Vue as _Vue } from 'vue/types/vue'
 import { VueModule } from 'vue-modules'
-import Router from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import VueRouter from 'vue-router'
 import { Store } from 'vuex'
 import App from './components/App.vue'
 
 export class RouterModule implements VueModule {
   readonly name = 'router'
-  router?: Router;
+  router?: ReturnType<typeof createRouter>;
   install(Vue: typeof _Vue) {
-    Vue.use(Router)
-
-    this.router = new Router({
+    this.router = createRouter({
+      history: createWebHistory(process.env.BASE_URL),
       mode: 'history',
       base: process.env.BASE_URL
     })
